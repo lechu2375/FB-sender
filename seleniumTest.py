5a3aeaa97434b8d138665fbccef906323a56ed1a
Lechu2375 <adamos2375@gmail.com>
2023-09-17 18:40:01 +0200

    update

--- seleniumTest.py
+++ seleniumTest.py
@@ -4,26 +4,47 @@ from selenium import webdriver
 from time import sleep
 import os
 import openai
+import json
+
 #gpt config
-openai.organization = "org-YpsxlYOd3L4eGJyQLBfPrt40"
-openai.api_key = "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"
+
 
 
 
 
 
 driver = webdriver.Chrome()
-driver.get('https://www.facebook.com/messages/t/100003896509329')
+driver.get('https://www.facebook.com/messages/t/100003896509329') #odpali się od razu po zalogowaniu na fb
+
+
+
+
+silesian_sys = 'Jesteś mieszkańcem Śląska, który używa tylko gwary Śląskiej. Podczas odpowiadania ubrawiaj odpowiedź krótkimi zmyślonymi historiami. Odpowiedź ma być krótka i zapisana w gwarze śląskiej.'
+offensive_sys = 'Jesteś bardzo wulgarnym komikiem, który zarabia odpowiadając wulgranie na wiadomości innych ludzi przy tym śmiejąc się z nich. Masz przeklinać i obrażać rozmówcę. Twoje wiadomości są krótkie, maksymalnie 40 słów.'
+def Send_GPT(text:str):
+
+    response = openai.ChatCompletion.create(
+              model="gpt-4",
+              messages=[{"role": "system", "content": offensive_sys},
+                        {"role": "user", "content": text}
+              ])
+    return response["choices"][0]["message"]["content"]
+
+
+
+# Otwórz plik tekstowy w trybie do odczytu+
+silesian_dictionary = list()
+with open("newslownik.json", "r", encoding="utf-8") as file:
+    silesian_dictionary = json.load(file)
+
+
+
+
+
 
 #Sends text to the actual messenger talker
 # Definicja funkcji Send_Text, która wysyła tekst na stronie za pomocą przeglądarki.
-def Send_Text(text:str): 
-    # Znajduje przycisk do wysyłania wiadomości na stronie.
-    send_button = driver.find_element(By.CSS_SELECTOR,"div[aria-label='Wiadomość']")
-    # Wprowadza tekst do pola wysyłania wiadomości.
-    send_button.send_keys(text)
-    # Symuluje naciśnięcie klawisza "Enter" aby wysłać wiadomość.
-    send_button.send_keys(Keys.RETURN)
+
 
 # Prosi użytkownika o zalogowanie się na Facebook.
 input("Zaloguj się na FB i kliknij klawisz...")
@@ -38,10 +59,20 @@ program_alive = True
 # Ustala początkową wartość najnowszej wiadomości na pustą.
 newest_message = ""
 
+def Send_Text(text:str): 
+    # Znajduje przycisk do wysyłania wiadomości na stronie.
+    send_button = driver.find_element(By.CSS_SELECTOR,"div[aria-label='Wiadomość']")
+    # Wprowadza tekst do pola wysyłania wiadomości.
+    send_button.send_keys(text)
+    # Symuluje naciśnięcie klawisza "Enter" aby wysłać wiadomość.
+    send_button.send_keys(Keys.RETURN)
+
+
 # Główna pętla programu, która działa dopóki program jest aktywny.
+
 while program_alive:
-    print("Myślę...")
-    sleep(5.0)
+    print("\n Myślę...")
+    sleep(10.0)
     old_message = newest_message
     # Pobiera najnowszą wiadomość z strony.
     newest_message =  driver.find_elements(By.CSS_SELECTOR,"[style*='background-color: var(--wash)']")[-1].text
@@ -49,17 +80,13 @@ while program_alive:
     if(newest_message == old_message):
         continue
 
-    to_send='Odpowiedz na wiadomość:'+newest_message
+    to_send=newest_message
     # Używa modelu GPT-4 do wygenerowania odpowiedzi na wiadomość.
-    response = openai.ChatCompletion.create(
-              model="gpt-4",
-              messages=[{"role": "system", "content": 'Jesteś wulgarnym mieszkańcem biednej dzielnicy na Śląsku '},
-                        {"role": "user", "content": to_send}
-              ])
-    response_text = response["choices"][0]["message"]["content"]
+    print(f"Console->GPT:{to_send}")
+    response_text = Send_GPT(to_send)
     # Wyświetla i wysyła wygenerowaną odpowiedź
     print("\n")
-    print(response_text) 
+    print(f"GPT->Console:{response_text}")
     Send_Text(response_text)
     print("\n")
     
